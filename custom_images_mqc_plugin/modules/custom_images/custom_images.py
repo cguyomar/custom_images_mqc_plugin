6057b49edc3710158b8c319125c3de2c223c3211
cguyomar <cervin.guyomar@gmail.com>
2021-06-17 15:58:51 +0200

    Deal with absolute/relative paths

--- custom_images_mqc_plugin/modules/custom_images/custom_images.py
+++ custom_images_mqc_plugin/modules/custom_images/custom_images.py
@@ -16,25 +16,35 @@ class MultiqcModule(BaseMultiqcModule):
 
         # Parse logs
         for f in self.find_log_files("custom_images/images_tab", filehandles=True):
+
             for line in f['f']:
                 line = line.split("\t")
                 if len(line) < 3:
                     log.warning("Ignoring badly formatted line : "+" ".join(line))
                 else:
                     image_file = line[0]
+                    image_file = os.path.expanduser(image_file)
                     image_text = line[1]
                     image_description = line[2]
 
-                    if os.path.isfile(image_file):
-                        self.make_section(image_file,image_text,image_description)
-                    else:
-                        log.warning("Image "+image_file+" does not exist")
+                    if os.path.isabs(image_file):
+                        image_path = image_file
 
-    def make_section(self,image_file,image_text,image_description):
+                    else:
+                        image_path = os.path.join(f["root"], image_file)
+
+                    log.info(image_path)
+
+                    if os.path.isfile(image_path):
+                        self.make_section(image_path,image_text,image_description)
+                    else:
+                        log.warning("Image "+image_path+" does not exist")
+
+    def make_section(self,image_path,image_text,image_description):
 
         self.add_section(
             name = image_text,
             description = image_description,
             anchor = "custom_images_1",
-            content = '<div class="mqc-custom-content-image"><img src="{}" /></div>'.format(image_file)
+            content = '<div class="mqc-custom-content-image"><img src="{}" /></div>'.format(image_path)
         )
